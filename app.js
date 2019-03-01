81978dbd3bffc9a8f6a3d62a05698d6be704abe0
Ritik Srivastava <ritik.space@gmail.com>
2019-03-01 14:57:58 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const app = require("express")();
+const mongoose = require("mongoose");
+const clear = require('clear');
+
+var call = 0;
+app.set("view engine", "ejs");
+
+const dbOptions = { useNewUrlParser: true, reconnectTries: Number.MAX_VALUE, poolSize: 10 };
+mongoose.connect(require("./mongo"), dbOptions).then(
+    () => { console.log(">  Connection Established"); },
+    e => { console.log(">  Connection Failed \n>  " + e); }
+);
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get("/*",function(req, res) {
+    res.send(req.originalUrl);
+});
+
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    clear();
+    console.log("\n" + ++call + ") Starting Server");
+    console.log(">  Server is Listening");
+    console.log("\n" + ++call + ") Connection to MongoDB Atlas Server");
+});
