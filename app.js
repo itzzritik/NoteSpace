be3086b8628468cb77dd24e15bbbe25fafc76dce
Ritik Srivastava <ritik.space@gmail.com>
2019-03-03 17:20:52 +0000

    Saving Values

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
 const clear = require('clear');
 const git = require('simple-git/promise')();
@@ -14,6 +15,7 @@ mongoose.connect(require("./mongo"), dbOptions).then(
     e => { console.log(">  Connection Failed \n>  " + e); }
 );
 
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
