31d06ad3d6e2a9b058af75ab51d62a6dde498a46
Ritik Srivastava <ritik.space@gmail.com>
2019-06-21 18:27:19 +0530

    Request Limit Increased

--- app.js
+++ app.js
@@ -40,7 +40,8 @@ var mongoConnect = function(callback) {
 };
 mongoConnect();
 
-app.use(bodyparser.json());
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
